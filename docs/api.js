de2de81669f4391bde06deeb609a4fcb930f744f
Marcel Erz <erz@yahoo-inc.com>
2014-11-04 12:53:48 -0800

    Add API-documentation and Coverage report

---
+++ docs/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractClient",
+        "AbstractClientDecorator",
+        "AbstractForkTask",
+        "AbstractTask",
+        "AbstractTaskDecorator",
+        "Client",
+        "Config",
+        "Coverage",
+        "CucumberClient",
+        "CucumberTask",
+        "GroupTask",
+        "GroupTaskDecorator",
+        "IdentifierTaskDecorator",
+        "KoboldClient",
+        "KoboldTask",
+        "MochaClient",
+        "MochaTask",
+        "NodeClient",
+        "NodeTask",
+        "PlainClientDecorator",
+        "PreceptorManager",
+        "ShellTask"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
